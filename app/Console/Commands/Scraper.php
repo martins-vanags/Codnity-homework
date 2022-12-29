36ca36b8a701ac62cac9b258492283296a12a69d
Bonuxs <vanagsm4@gmail.com>
2022-12-30 00:41:44 +0200

    Add scraper

---
+++ app/Console/Commands/Scraper.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Articles;
+use Goutte\Client;
+use Illuminate\Console\Command;
+
+class Scraper extends Command
+{
+    protected $signature = 'scrape:hacker-news';
+
+    protected $description = 'Scrape https://news.ycombinator.com/ and store title,link,points,date created';
+
+    public function handle()
+    {
+        $client = new Client();
+        $crawler = $client->request('GET', 'https://news.ycombinator.com/');
+
+        $titles = $crawler->filter('span.titleline > a')->extract(['_text']);
+        $link = $crawler->filter('span.titleline > a')->extract(['href']);
+        $points = $crawler->filter('.score')->extract(['_text']);
+        $dateCreated = $crawler->filter('.age')->extract(['title']);
+
+        for ($i = 0; $i < count($titles); $i++) {
+            $title = array_key_exists($i, $titles) ? $titles[$i] : null;
+
+            $article = Articles::where('title', $title);
+
+            if ($article->exists()) {
+                $article->update([
+                    'points' => array_key_exists($i, $points) ? $points[$i] : null,
+                ]);
+            } else {
+                Articles::create([
+                    'title' => $title,
+                    'points' => array_key_exists($i, $points) ? $points[$i] : null,
+                    'link' => array_key_exists($i, $link) ? $link[$i] : null,
+                    'created_at' => array_key_exists($i, $dateCreated) ? $dateCreated[$i] : null,
+                ]);
+            }
+        }
+
+
+    }
+}
---
+++ app/Models/Articles.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class Articles extends Model
+{
+    use SoftDeletes, RefreshDatabase;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'title',
+        'points',
+        'created_at',
+        'updated_at',
+        'link',
+    ];
+
+}
